f4b62760f314f9d9c13f2d74b5f8ac9a0254bb6a
ShivKokkula <shivbk987@gmail.com>
2021-05-04 09:23:36 +0530

    Refactor - implemented employee attendace feature in empWageComputation.sh file

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program";
+
+isPresent=1;
+
+randomCheck=$(( RANDOM%2 ));
+
+if [ $randomCheck -eq $isPresent ];
+then
+    echo "Employee is present";
+else
+    echo "Employee is not present";
+fi
+
