135c2f7816b4d48874cf5cef4b34a8ffce974c3a
ShivKokkula <shivbk987@gmail.com>
2021-05-05 08:46:43 +0530

    Refactor - implemented Daily wage calculation feature in UC-1-DailyEmployeeWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,7 +9,11 @@ randomCheck=$(( RANDOM%2 ));
 if [ $randomCheck -eq $isPresent ];
 then
     echo "Employee is present";
+	 empRateperHr=20;
+    empHrs=8;
+    salary=$(( $empRateperHr * $empHrs));
 else
     echo "Employee is not present";
+	 salary=0;
 fi
 
